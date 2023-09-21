005a00b38e96e6dff4426aba1bb79ed831a93772
LambdaC <studycwg@gmail.com>
2023-09-21 23:27:46 +0800

    update babylonjs packages

--- src/core.ts
+++ src/core.ts
@@ -1,9 +1,10 @@
-import "babylonjs";
+import { ArcRotateCamera, AxesViewer, Camera, Color3, Engine, PointLight, Scene, Texture, Vector3 } from "@babylonjs/core";
+import { StarfieldProceduralTexture } from "@babylonjs/procedural-textures";
 
-export function createStartScene(engine: BABYLON.Engine): { scene: BABYLON.Scene, camera: BABYLON.Camera } {
-    const scene = new BABYLON.Scene(engine);
-    const camera = new BABYLON.ArcRotateCamera('camera1', 0, Math.PI / 4, 100, BABYLON.Vector3.Zero(), scene);
-    const axes = new BABYLON.Debug.AxesViewer(scene, 10);
+export function createStartScene(engine: Engine): { scene: Scene, camera: Camera } {
+    const scene = new Scene(engine);
+    const camera = new ArcRotateCamera('camera1', 0, Math.PI / 4, 100, Vector3.Zero(), scene);
+    const axes = new AxesViewer(scene, 10);
     setupEnvironment(scene);
     createStar(scene);
     populatePlanetarySystem(scene);
@@ -11,14 +12,28 @@ export function createStartScene(engine: BABYLON.Engine): { scene: BABYLON.Scene
     return { scene, camera };
 }
 
-function setupEnvironment(scene: BABYLON.Scene) {
+function setupEnvironment(scene: Scene) {
+    const starfieldPT = new StarfieldProceduralTexture("starfieldPT", 512, scene);
+    starfieldPT.coordinatesMode = Texture.FIXED_EQUIRECTANGULAR_MIRRORED_MODE;
+    starfieldPT.darkmatter = 1.5;
+    starfieldPT.distfading = 0.75;
+    const envOptions = {
+        skyboxSize: 512,
+        createGround: false,
+        skyboxTexture: starfieldPT,
+        environmentTexture: starfieldPT
+    };
+    const light = new PointLight("starLight", Vector3.Zero(), scene);
+    light.intensity = 2;
+    light.diffuse = new Color3(.98, .9, 1);
+    light.specular = new Color3(1, 0.9, 0.5);
+    let env = scene.createDefaultEnvironment(envOptions);
+}
+
+function createStar(scene: Scene) {
 
 }
 
-function createStar(scene: BABYLON.Scene) {
-
-}
-
-function populatePlanetarySystem(scene: BABYLON.Scene) {
+function populatePlanetarySystem(scene: Scene) {
 
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import 'babylonjs';
+import { Engine } from '@babylonjs/core';
 import { createStartScene } from '@/core';
 
 const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement; // Get the canvas element
-const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+const engine = new Engine(canvas, true); // Generate the BABYLON 3D engine
 const createScene = function () {
     const startScene = createStartScene(engine);
     return startScene.scene;

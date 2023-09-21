c0e3e4adfca91a2b4b7570abbab79076e08e52ea
LambdaC <studycwg@gmail.com>
2023-09-21 13:49:19 +0800

    init

---
+++ src/core.ts
@@ -0,0 +1,5 @@
+import { Engine, Scene } from "babylonjs";
+
+export const createStartScene = (engine: Engine): { scene: Scene } => {
+
+}
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import * as BABYLON from 'babylonjs';
+import { createStartScene } from '@/core';
+
+const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement; // Get the canvas element
+const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+const createScene = function () {
+    const startScene = createStartScene(engine);
+    return startScene.scene;
+};
+const scene = createScene(); //Call the createScene function
+// Register a render loop to repeatedly render the scene
+engine.runRenderLoop(function () {
+    scene.render();
+});
+// Watch for browser/canvas resize events
+window.addEventListener("resize", function () {
+    engine.resize();
+});
